4c757a02d9984848e181f0b4b431ba0a0aec080b
anjoserohijos-programming <aserohijos88@gmail.com>
2025-10-06 07:28:14 +0800

    [add]: basic manager files

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_course/presentation/font_manager.dart';
+
+TextStyle _getTextStyle(double fontSize, String fontFamily, Color color, FontWeight fontWeight) {
+  return TextStyle(fontSize: fontSize, fontFamily: fontFamily, color: color, fontWeight: fontWeight);
+}
+
+TextStyle getRegularStyle({double fontSize = FontSizeManager.medium, required Color color}) {
+  return _getTextStyle(fontSize, FontConstants.fontFamily, color, FontWeightManager.medium);
+}
+
+TextStyle getLightStyle({double fontSize = FontSizeManager.medium, required Color color}) {
+  return _getTextStyle(fontSize, FontConstants.fontFamily, color, FontWeightManager.light);
+}
+
+TextStyle getBoldStyle({double fontSize = FontSizeManager.medium, required Color color}) {
+  return _getTextStyle(fontSize, FontConstants.fontFamily, color, FontWeightManager.bold);
+}
